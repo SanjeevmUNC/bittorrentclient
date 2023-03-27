ad124ad227c03c2374c0a00c8683854085f9832f
SanjeevmUNC <54512437+SanjeevmUNC@users.noreply.github.com>
2023-03-27 13:33:57 -0400

    Add files via upload

---
+++ tracker.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
+
+module.exports.getPeers = (torrent, callback) => {
+  const socket = dgram.createSocket('udp4');
+  const url = torrent.announce.toString('utf8');
+
+  // 1. send connect request
+  udpSend(socket, buildConnReq(), url);
+
+  socket.on('message', response => {
+    if (respType(response) === 'connect') {
+      // 2. receive and parse connect response
+      const connResp = parseConnResp(response);
+      // 3. send announce request
+      const announceReq = buildAnnounceReq(connResp.connectionId);
+      udpSend(socket, announceReq, url);
+    } else if (respType(response) === 'announce') {
+      // 4. parse announce response
+      const announceResp = parseAnnounceResp(response);
+      // 5. pass peers to callback
+      callback(announceResp.peers);
+    }
+  });
+};
+
+function udpSend(socket, message, rawUrl, callback=()=>{}) {
+  const url = urlParse(rawUrl);
+  socket.send(message, 0, message.length, url.port, url.host, callback);
+}
+
+function respType(resp) {
+  // ...
+}
+
+const crypto = require('crypto'); // 1
+
+function buildConnReq() {
+    const buf = Buffer.alloc(16); // 2
+
+    // connection id
+    buf.writeUInt32BE(0x417, 0); // 3
+    buf.writeUInt32BE(0x27101980, 4);
+    // action
+    buf.writeUInt32BE(0, 8); // 4
+    // transaction id
+    crypto.randomBytes(4).copy(buf, 12); // 5
+  
+    return buf;}
+
+function parseConnResp(resp) {
+  // ...
+}
+
+function buildAnnounceReq(connId) {
+  // ...
+}
+
+function parseAnnounceResp(resp) {
+  // ...
+}
