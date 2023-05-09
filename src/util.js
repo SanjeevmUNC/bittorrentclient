3232b35dd7b1130b01987fea97603a290732ee00
Cody Bollinger <cobol373@live.unc.edu>
2023-05-09 15:01:51 -0400

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,10 +1,8 @@
-'use strict';
-
-const crypto = require('crypto');
+import crypto from "crypto";
 
 let id = null;
 
-module.exports.genId = () => {
+export function genId() {
   if (!id) {
     id = crypto.randomBytes(20);
     Buffer.from('-AT0001-').copy(id, 0);
